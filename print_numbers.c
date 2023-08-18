7c461a2e69250c003daec089dd9be67507a2fcaf
Omar-Driouch <omardriouch29@gmail.com>
2023-08-19 00:47:21 +0100

    the fucntion of binary is tested and its working

--- print_numbers.c
+++ print_numbers.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+/**
+ * printf_binary - Custom printf conversion for binary integer printing.
+ *
+ * Takes a va_list with an integer, converts it to binary
+ * and prints the reversed binary string.
+ *
+ * @ls:ls - va_list with the integer to convert and print.
+ *
+ * Return:Result of the print_rev function, indicating printed characters count.
+ */
+
 int printf_binary(va_list ls)
 {
 	int num = va_arg(ls, int);
